d202b80cfa974547fe4c3f74c98e6b2f1930eb32
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-06-13 03:13:01 -0500

    Added sw

---
+++ web/sw.js
@@ -0,0 +1,25 @@
+var CACHE_NAME = 'pdf-reader-v1';
+var urlsToCache = [
+    '/web/viewer.html'
+];
+
+// Install handler
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch handler
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
