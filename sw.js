1b1a845f9dcaa97d1cf550dcb8396c51b3f54a64
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-06-20 23:06:23 -0500

    More files to sw and changed scope

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
 var CACHE_NAME = 'pdf-reader-v2';
 var urlsToCache = [
-    '/pdf-reader/web/viewer.html'
+    '/pdf-reader/web/viewer.html',
+    '/pdf-reader/web/viewer.css',
+    '/pdf-reader/public/img/favicon.ico',
+    '/pdf-reader/public/img/manifest.json',
+    '/pdf-reader/build/pdf.js',
+    '/pdf-reader/build/pdf.worker.js'
 ];
 
 // Install handler
